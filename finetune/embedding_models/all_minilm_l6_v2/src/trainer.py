eb1da909e40f94b340335ec298677c4228bca30d
LTNhoTin <tinle155@gmail.com>
2025-10-13 16:58:28 +0700

    ok

--- finetune/embedding_models/all_minilm_l6_v2/src/trainer.py
+++ finetune/embedding_models/all_minilm_l6_v2/src/trainer.py
@@ -557,7 +557,7 @@ class MiniLMTrainer:
         import yaml
         
         # Load target metrics from config.yaml
-        config_path = Path(__file__).parent.parent.parent / "config.yaml"
+        config_path = Path(__file__).parent.parent / "config.yaml"
         with open(config_path, 'r') as f:
             config_data = yaml.safe_load(f)
         
